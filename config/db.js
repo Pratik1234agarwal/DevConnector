190f2f17f0165f7ff4c866c1c34befc30b46288a
Pratik Agarwal <pratik1234agarwal@gamil.com>
2020-08-28 19:59:06 +0530

    Prepared files for deployment

---
+++ config/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.connect(db);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useNewUrlParser: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { response } = require('express');
+
+module.exports = function (req, res, next) {
+  // Get the token from the header
+  const token = req.header('x-auth-token');
+
+  // Check if the header has a token
+  if (!token) {
+    return res.status(401).json({ message: 'No token auth denied' });
+  }
+
+  // Verify the token sent in the header (check for validity)
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Token is not valid' });
+  }
+};
